8e504a1e694754bec05f4606ac295fa7b0e528de
Federico Bayerque <fbayerque@alumnos.exa.unicen.edu.ar>
2022-08-17 20:06:23 -0300

    Updated controllers with new authentication admin

--- src/main/java/com/portfolio/fb/repository/EducationRepo.java
+++
@@ -1,9 +0,0 @@
-package com.portfolio.fb.repository;
-
-import com.portfolio.fb.model.Education;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EducationRepo extends JpaRepository<Education,Long> {
-}
--- src/main/java/com/portfolio/fb/repository/ExtraSkillRepo.java
+++
@@ -1,9 +0,0 @@
-package com.portfolio.fb.repository;
-
-import com.portfolio.fb.model.ExtraSkill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExtraSkillRepo extends JpaRepository<ExtraSkill,Long> {
-}
