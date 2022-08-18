b47120f1fac873e63c646dc5a30393a6fd61a2f8
Federico Bayerque <fbayerque@alumnos.exa.unicen.edu.ar>
2022-08-18 09:52:41 -0300

    Refactor jwt and secured routes

--- src/main/java/com/portfolio/fb/security/dto/JwtDto.java
+++ src/main/java/com/portfolio/fb/security/dto/JwtDto.java
@@ -2,20 +2,15 @@ package com.portfolio.fb.security.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
 
 @Getter @Setter
 public class JwtDto {
     private String token;
-    private String bearer = "Bearer";
-    private String email;
-    private Collection<? extends GrantedAuthority> authorities;
 
-    public JwtDto(String token, String email, Collection<? extends GrantedAuthority> authorities) {
+    public JwtDto() {
+    }
+
+    public JwtDto(String token) {
         this.token = token;
-        this.email = email;
-        this.authorities = authorities;
     }
 }
--- src/main/java/com/portfolio/fb/security/jwt/JwtProvider.java
+++ src/main/java/com/portfolio/fb/security/jwt/JwtProvider.java
@@ -6,9 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class JwtProvider {
@@ -21,20 +23,23 @@ public class JwtProvider {
 
     public String generateToken(Authentication authentication){
         Admin admin = (Admin) authentication.getPrincipal();
-        return Jwts.builder().setSubject(admin.getUsername())
+        List<String> roles = admin.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+        return Jwts.builder()
+                .setSubject(admin.getUsername())
+                .claim("roles",roles)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime() + expiration * 1000))
-                .signWith(SignatureAlgorithm.HS256,secret)
+                .signWith(SignatureAlgorithm.HS256,secret.getBytes())
                 .compact();
     }
 
     public String getEmailFromToken(String token){
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token){
         try {
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token);
             return true;
         }catch (MalformedJwtException e){
             logger.error("token malformed");
